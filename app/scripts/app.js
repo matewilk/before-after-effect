7d65f9005a874f2c6739c0d6bbfa64bc1e1c67c6
Mateusz Wilk <matewilk@gmail.com>
2014-07-29 23:59:20 +0100

    angular-responsive strucutre implementation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,26 +8,31 @@
  *
  * Main module of the application.
  */
-angular
-  .module('prototypeApp', [
-    'ngAnimate',
+angular.module('prototypeApp', ['ngRoute', 'angular-responsive', 'ngAnimate',
     'ngCookies',
     'ngResource',
-    'ngRoute',
     'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+    'ngTouch'])
+  .config(function ($routeProvider, responsiveHelperProvider) {
+
+        var device = 'desktop';
+        var responsiveHelper = responsiveHelperProvider.$get();
+        if(responsiveHelper.isMobile()){
+            device = 'mobile';
+        }
+        $routeProvider.when('/view1', {templateUrl: 'views/' + device + '/view1.html'});
+        $routeProvider.when('/view2', {templateUrl: 'views/' + device + '/view2.html'});
+        $routeProvider.otherwise({redirectTo: '/view1'});
+//    $routeProvider
+//      .when('/', {
+//        templateUrl: 'views/main.html',
+//        controller: 'MainCtrl'
+//      })
+//      .when('/about', {
+//        templateUrl: 'views/about.html',
+//        //controller: 'AboutCtrl'
+//      })
+//      .otherwise({
+//        redirectTo: '/'
+//      });
   });
